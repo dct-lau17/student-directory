dd685819f73e40afb289497313bd1233b2a5dc49
Daniel <dct.lau17@gmail.com>
2018-06-04 20:26:06 +0100

    addition of student number before student name

--- directory.rb
+++ directory.rb
@@ -1,39 +1,39 @@
-def input_student
+def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   # create an empty array
   students = []
-  # get the first name of the array
+  # get the first name
   name = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
-  	#add the student has to the array
-  	students << {name: name, cohort: :november}
-  	puts "Now we have #{students.count} students"
-  	# get another name from user
-  	name = gets.chomp
-    end
-    #return the array of students
-    students
- end
+    # add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
 
 def print_header
-  puts "The student of Villains Academy"
-  puts "--------------"
+  puts "The students of Villains Academy"
+  puts "-------------"
 end
 
 def print(students)
-  students.each do |student|
-		puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each_with_index do |student, idx|
+    puts " #{idx + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
 end
 
-# nothing happens until we call the methods
-students = input_student
+students = input_students
+#nothing happens until we call the methods
 print_header
 print(students)
 print_footer(students)

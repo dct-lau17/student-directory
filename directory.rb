8ae8d8ad3d64a78fc1308f691c6ccc39b7c290bf
Daniel <dct.lau17@gmail.com>
2018-06-04 20:36:56 +0100

    additional student information (height and hobby)

--- directory.rb
+++ directory.rb
@@ -1,40 +1,48 @@
-def input_students
-  puts "Please enter the names of the students"
+def input_student
+  
   puts "To finish, just hit return twice"
   # create an empty array
   students = []
-  # get the first name
-  name = gets.chomp
+ 
   # while the name is not empty, repeat this code
-  while !name.empty? do
-    # add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
-    # get another name from the user
+  while true do
+    puts "Please enter the names of the student"
     name = gets.chomp
-  end
-  # return the array of students
-  students
-end
+    break if name.empty?
+    puts  "Please enter the students hobby"
+    hobby = gets.chomp
+    puts "Please enter the height"
+    height = gets.chomp
+
+    #add the student has to the array
+    students << {name: name, cohort: :november, hobby: hobby, height: height}
+    puts "Now we have #{students.count} students"
+    # get another name from user
+    break if name.empty? 
+    end
+    #return the array of students
+    students
+ end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The Student of Villains Academy"
+  puts "----------------------------"
 end
 
 def print(students)
   counter = 0
   while counter < students.length do
-    puts "#{counter + 1}. #{students[counter][:name]} (#{students[counter][:cohort]})" 
+    puts "#{counter + 1}. #{students[counter][:name]} (#{students[counter][:cohort]} cohort) is #{students[counter][:height]}cm tall with an interest in #{students[counter][:hobby]} "
     counter += 1
   end
 end
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
 end
 
-students = input_students
-#nothing happens until we call the methods
+# nothing happens until we call the methods
+students = input_student
 print_header
 print(students)
 print_footer(students)

7ba1af755e51802af23b59dbc01d1473a1a31325
Daniel <dct.lau17@gmail.com>
2018-06-06 22:48:49 +0100

    Ability to load data from students.csv

--- directory.rb
+++ directory.rb
@@ -106,6 +106,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -124,6 +125,8 @@ def process(selection)
 		show_students 
     when "3"
     	save_students
+    when "4"
+    	load_students
 	when "9"
 		exit
 	else
@@ -141,5 +144,14 @@ def save_students
   end
   file.close
 end	
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort, hobby, height = line.chomp.split(",")
+      @students << {name: name, cohort: cohort.to_sym, hobby: hobby, height: height}
+  end
+  file.close	
+end
 
 interactive_menu
